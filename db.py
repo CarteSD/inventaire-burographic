21f011d11d1ab8239f6589b1287b1ca612c018e0
Estéban DESESSARD <e.desessard@burographic.fr>
2025-05-12 14:17:48 +0200

    📝 Utilisation du format docstrings pour le fichier `db.py`

--- db.py
+++ db.py
@@ -1,15 +1,25 @@
-# # # # # # # # # # # #
-# But : Ce fichier contient l'ensemble des méthodes relatives à la base de données
-# Par : Estéban DESESSARD - e.desessard@burographic.fr
-# Date : 11/04/2025
-# # # # # # # # # # # #
+"""
+    But : Ce fichier contient l'ensemble des méthodes relatives à la base de données
+    Par : Estéban DESESSARD - e.desessard@burographic.fr
+    Date : 11/04/2025
+"""
 
 import pyodbc
 from utils import *
 
-# But : Créer une connexion avec la base de données.
-#       Retourne la connexion si celle réussie, None dans le cas contraire
 def database_connection():
+    """
+    Crée une connexion à la base de données.
+
+    Args:
+        Aucun
+    
+    Returns:
+        connection (pyodbc.Connection): La connexion à la base de données si réussie, None sinon.
+
+    Raises:
+        pyodbc.Error: Si une erreur se produit lors de la connexion à la base de données.
+    """
     try:
         if DB_USER and DB_PASSWORD:
             connection = pyodbc.connect(
@@ -35,8 +45,20 @@ def database_connection():
         print(f"Erreur lors de la connexion à SQL Server : {e}")
         return None
 
-# But : Vérifier si un article existe dans la base de données
 def article_exists(connection, num_commercial):
+    """
+    Vérifie si un article existe dans la base de données.
+    
+    Args:
+        connection (pyodbc.Connection): La connexion à la base de données.
+        num_commercial (str): Le numéro commercial de l'article à vérifier.
+
+    Returns:
+        bool: True si l'article existe, False sinon.
+
+    Raises:
+        pyodbc.Error: Si une erreur se produit lors de la requête.
+    """
     try:
         cursor = connection.cursor()
         query = "SELECT COUNT(*) FROM ElementDef WHERE NumCommercialGlobal = ?"
@@ -48,8 +70,20 @@ def article_exists(connection, num_commercial):
         write_log(f"[ERREUR] {str(e)}")
         return False
 
-# But : Récupérer le code de la famille d'un article
 def get_family(connection, num_commercial):
+    """
+    Récupère les informations d'une famille à partir du numéro commercial d'un article.
+
+    Args:
+        connection (pyodbc.Connection): La connexion à la base de données.
+        num_commercial (str): Le numéro commercial de l'article.
+
+    Returns:
+        tuple: Un tuple contenant le code de la famille et son libellé, ou None si l'article n'existe pas.
+
+    Raises:
+        pyodbc.Error: Si une erreur se produit lors de la requête.
+    """
     try:
         cursor = connection.cursor()
         query = "SELECT FA.Code, FA.Libelle FROM FamilleArticle FA JOIN ElementDef ED ON FA.Code = ED.Famille WHERE ED.NumCommercialGlobal = ?"
@@ -64,8 +98,20 @@ def get_family(connection, num_commercial):
         write_log(f"[ERREUR] {str(e)}")
         return None
 
-# But : Récupérer le nom de la famille d'un article à partir de son code
 def get_family_name(connection, family_code):
+    """
+    Récupère le nom de la famille à partir de son code.
+    
+    Args:
+        connection (pyodbc.Connection): La connexion à la base de données.
+        family_code (str): Le code de la famille.
+        
+    Returns:
+        str: Le nom de la famille si trouvé, None sinon.
+        
+    Raises:
+        pyodbc.Error: Si une erreur se produit lors de la requête.
+    """
     try:
         cursor = connection.cursor()
         query = "SELECT Libelle FROM FamilleArticle WHERE Code = ?"
@@ -80,8 +126,19 @@ def get_family_name(connection, family_code):
         write_log(f"[ERREUR] {str(e)}")
         return None
 
-# But : Récupérer tous les articles de stock en base de données
 def get_all_articles(connection):
+    """
+    Récupère tous les articles de stock à partir de la base de données.
+    
+    Args:
+        connection (pyodbc.Connection): La connexion à la base de données.
+        
+    Returns:
+        list: Une liste de tuples contenant les informations des articles, ou None si aucun article n'est trouvé.
+
+    Raises:
+        pyodbc.Error: Si une erreur se produit lors de la requête.
+    """
     try:
         cursor = connection.cursor()
         query = "SELECT ES.*, ED.NumCommercialGlobal FROM ElementStock ES JOIN ElementDef ED ON ES.CodeElem = ED.Code"
@@ -96,8 +153,20 @@ def get_all_articles(connection):
         write_log(f"[ERREUR] {str(e)}")
         return None
 
-# But : Récupérer le stock d'un article à partir de son numéro commercial
 def get_article_stock(connection, commercial_num):
+    """
+    Récupère le stock d'un article à partir de son numéro commercial.
+    
+    Args:
+        connection (pyodbc.Connection): La connexion à la base de données.
+        commercial_num (str): Le numéro commercial de l'article.
+        
+    Returns:
+        tuple: Un tuple contenant les informations de stock de l'article, ou None si l'article n'existe pas.
+        
+    Raises:
+        pyodbc.Error: Si une erreur se produit lors de la requête.
+    """ 
     try:
         cursor = connection.cursor()
         query = "SELECT ES.*, ED.NumCommercialGlobal, ED.LibelleStd FROM ElementStock ES JOIN ElementDef ED ON ES.CodeElem = ED.Code WHERE ED.NumCommercialGlobal = ?"
@@ -112,8 +181,20 @@ def get_article_stock(connection, commercial_num):
         write_log(f"[ERREUR] {str(e)}")
         return None
     
-# But : Récupérer la définition d'un article
 def get_article_name(connection, commercial_num):
+    """
+    Récupère le nom d'un article à partir de son numéro commercial.
+    
+    Args:
+        connection (pyodbc.Connection): La connexion à la base de données.
+        commercial_num (str): Le numéro commercial de l'article.
+        
+    Returns:
+        str: Le nom de l'article si trouvé, None sinon.
+        
+    Raises:
+        pyodbc.Error: Si une erreur se produit lors de la requête.
+    """
     try:
         cursor = connection.cursor()
         query = "SELECT LibelleStd FROM ElementDef WHERE NumCommercialGlobal = ?"
@@ -128,11 +209,24 @@ def get_article_name(connection, commercial_num):
         write_log(f"[ERREUR] {str(e)}")
         return None
 
-# But : Créer un mouvement de stock sur un article défini
-#
-#       Afin d'assurer la cohérence des données, met à jour la table ElementStock
-#       pour garantir que les quantités restent cohérentes entre elles
 def create_movement(connection, movement_type, article, quantite):
+    """
+    Crée un mouvement de stock pour un article donné.
+    Afin de garantir la cohérence des données, met à jour la table ElementStock
+    pour s'assurer que les quantités restent cohérentes entre elles.
+
+    Args:
+        connection (pyodbc.Connection): La connexion à la base de données.
+        movement_type (str): Le type de mouvement ('E' pour entrée, 'S' pour sortie).
+        article (tuple): Les informations de l'article.
+        quantite (int): La quantité à ajouter ou à soustraire.
+
+    Returns:
+        bool: True si le mouvement a été créé avec succès, False sinon.
+
+    Raises:
+        pyodbc.Error: Si une erreur se produit lors de la requête.
+    """
     try:
         cursor = connection.cursor()
         # Insertion du mouvement de stock
